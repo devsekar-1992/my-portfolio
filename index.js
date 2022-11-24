63dc572151d666c6ddf8e5a71a1302e9ad0b4fdd
Abaid Ullah Aleem <103155286+Abaid78@users.noreply.github.com>
2022-11-24 05:00:00 -0800

    Add files via upload
    portfolio template

---
+++ index.js
@@ -0,0 +1,20 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var sidemenu=document.getElementById("sidemenu");
+var tab_contents=document.getElementsByClassName("tab-contents");
+function openTab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tab_content of tab_contents){
+        tab_content.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+function openmenue(){
+    sidemenu.style.right="0";
+}
+function closemenue(){
+    sidemenu.style.right="-200px";
+
+}
